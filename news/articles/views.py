7904785dd6b99c1eba8ddfe13e9bd3f71e636771
NVDungg <dung692000@gmail.com>
2022-11-22 15:58:51 +0700

    add CBV and template for article

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView, CreateView
+
+from django.views.generic import ListView, DetailView
+#use django.views.generic import UpdateView & DeleteView... if don't import forms.py or doens't have
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+
 from .models import Article
 from django.urls import reverse_lazy
 # Create your views here.
 
 class ArticleListView(ListView):
     model = Article
-    template_name = "article_list.html"
+    template_name = "articles/article_list.html"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "articles/article_detail.html"
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ("title", "body")
+    template_name = "articles/article_edit.html"
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "articles/article_delete.html"
+    success_url = reverse_lazy("article_list")
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "articles/article_new.html"
+    fields = ("title", "body", "author", )
